3957abf5f62811d438a83a67cc60433dd1be20cd
Gael-Bernard <gael.berbere@gmail.com>
2022-10-27 01:43:40 +0200

    Revert "Minor fixes in package.json"
    This reverts commit 2a89bde9e5e66a8f05a456f091536c76e736e47c.

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,13 @@ dotenv.config();
 
 import { Client, Events, GatewayIntentBits } from "discord.js"
 
-import declareOnMessageResponse from "./messageEvent/onMessage";
-
-
 const client = new Client({intents: [
   GatewayIntentBits.GuildMessages
 ]})
 
 
-// Whenever the bot gets online
 client.once(Events.ClientReady, c => {
   console.log(`${new Date()} | Bot logged in as ${c.user.tag}`);
-
-  // Declare the functions triggered by events
-  declareOnMessageResponse(client);
 });
 
 
