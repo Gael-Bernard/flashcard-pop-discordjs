e587acc42fabd5b190b669486813444e5d782a49
Gael-Bernard <gael.berbere@gmail.com>
2022-10-29 05:30:34 +0200

    Completed onMessage.ts to randomly send flashcards prompts

--- src/bot_messages/createFlashcardPopup.ts
+++ src/bot_messages/createFlashcardPopup.ts
@@ -4,8 +4,8 @@ import Flashcard from "../datastructures/Flashcard";
 export default function createFlashcardPopup(flashcard:Flashcard): EmbedBuilder {
   
   const description = 
-      "Front of your flashcard : "+flashcard.front
-      +"\n*Type the ``/notdefinedyet []`` command to tell me what you think is on the back!*"
+      "Front of your flashcard : **"+flashcard.front+"**"
+      +"\n\n*Type the ``/notdefinedyet [your-answer]`` command to tell me what you think is at the back!*"
   ;
   
   return new EmbedBuilder()
--- src/events/discordjs/onMessage.ts
+++ src/events/discordjs/onMessage.ts
@@ -1,4 +1,10 @@
-import { Client } from "discord.js";
+import { Client, Embed, EmbedBuilder } from "discord.js";
+
+import createFlashcardPopup from "../../bot_messages/createFlashcardPopup.js";
+import getFlashcardsOf from "../../database/getFlashcards.js";
+import Flashcard from "../../datastructures/Flashcard.js";
+
+/* NOTE Messages contain an empty msg.content, because a specific intent is necessary to access the content. Though we don't need it */
 
 /**
  * Declares the function triggered when a message is sent in the guild
@@ -7,12 +13,22 @@ import { Client } from "discord.js";
 export default function declareOnMessageResponse(client:Client) {
   client.on("messageCreate", msg => {
     
-    if(msg.author.bot)
+    if(msg.author.bot) // We only listen to users
       return;
     
-    console.log("Content : "+msg.content+" auth: "+msg.author); // Message not displayed because a 3rd intent is necessary. Though we don't need it
+    if(Math.random() < 0.5) // We randomly choose to give a flashcard or not
+      return;
     
+    const flashcards:Flashcard[] = getFlashcardsOf(msg.author.id);
+    if(flashcards.length == 0) { // If the user has no flashcard
+      return;
+    }
     
+    // Pick random flashcard
+    const flashcard:Flashcard = flashcards.at( Math.trunc( flashcards.length * Math.random() ) );
     
+    // Display
+    const embed:EmbedBuilder = createFlashcardPopup(flashcard);
+    msg.reply({ embeds:[embed] });
   });
 }
