c9551a63e21efd93a774ccc1b6af3e3617ac8a85
Gael-Bernard <gael.berbere@gmail.com>
2022-11-01 02:01:01 +0100

    Split FlashcardChannel into DB attributes and non-DB

--- src/datastructures/FlashcardChannel.ts
+++ src/datastructures/FlashcardChannel.ts
@@ -2,11 +2,18 @@ import Flashcard from "./Flashcard.js"
 
 
 /**
+ * Data of a FlashcardChannel stored in the database
+ */
+type FlashcardChannelForDB = {
+  uuid: string,
+  popProbability: number
+}
+
+
+/**
  * Type containing the information about a channel (ID, popup probability)
  */
- type FlashcardChannel = {
-  uuid: string,
-  popProbability: number,
+ type FlashcardChannel = FlashcardChannelForDB & {
   currentFlashcard?: Flashcard
 };
 export default FlashcardChannel;
