8d2f2ffb05a96405498b16ccf864ddbe1771e864
Gael-Bernard <gael.berbere@gmail.com>
2022-11-30 13:47:22 +0100

    Completed FlashcardUser.ts

--- src/database/FlashcardUser.ts
+++ src/database/FlashcardUser.ts
@@ -1,5 +1,14 @@
+import FlashcardCollection from "./FlashcardCollection.js";
+import UserAnswers from "./UserAnswers.js";
+
 export default interface FlashcardUser {
 
-  
+  getId(): number;
+
+  getFlashcardCollectionIds(): Promise<number[]>;
+  getFlashcardCollections(): Promise<FlashcardCollection[]>;
+
+  getCollectionStatistics(collectionId: number): Promise<UserAnswers>;
+  getAllCollectionStatistics(): Promise<UserAnswers[]>;
 
 }
